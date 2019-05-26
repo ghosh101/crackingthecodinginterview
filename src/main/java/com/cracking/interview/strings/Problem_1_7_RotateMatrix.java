66090d489ab449c61ab5c6281fc310fa1f5a7f46
Suman Ghosh <ghosh.10@gmail.com>
2019-05-26 12:56:57 -0400

    problem 7

---
+++ src/main/java/com/cracking/interview/strings/Problem_1_7_RotateMatrix.java
@@ -0,0 +1,25 @@
+package com.cracking.interview.strings;
+
+public class Problem_1_7_RotateMatrix {
+
+	public static void main(String[] args) {
+		int[][] matrix = {{1,2,3},{4,5,6}, {7,8,9}};
+
+		rotateMatrix(matrix);
+	}
+
+	private static int[][] rotateMatrix(int[][] matrix) {
+		
+		int[][] result = new int[matrix.length][matrix.length];
+		
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = matrix.length-1; j >= 0; j--) {
+				
+				result[j][matrix.length-1 - i] = matrix[i][j];
+			}
+		}
+		
+		return result;
+	}
+
+}
