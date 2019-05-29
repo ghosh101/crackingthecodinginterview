315ba7b8e9bb58595ccce3b0e28ff1f92d733bdd
Suman Ghosh <ghosh.10@gmail.com>
2019-05-28 22:47:30 -0400

    added comments

--- src/main/java/io/pramp/mock/Mock_1_Interviewer.java
+++ src/main/java/io/pramp/mock/Mock_1_Interviewer.java
@@ -2,6 +2,33 @@ package io.pramp.mock;
 
 import java.util.Stack;
 
+/* Implement a regular expression function isMatch that supports the '.' and '*' symbols. 
+ * The function receives two strings - text and pattern - and should return true if the text matches the pattern as a regular expression. 
+ * For simplicity, assume that the actual symbols '.' and '*' do not appear in the text string and are used as special symbols only in the pattern string.
+ * In case you aren’t familiar with regular expressions, the function determines if the text and pattern are the equal, 
+ * where the '.' is treated as a single a character wildcard (see third example), 
+ * and '*' is matched for a zero or more sequence of the previous letter (see fourth and fifth examples). 
+ *
+ * For more information on regular expression matching, see the Regular Expression Wikipedia page.
+ * 
+ * Explain your algorithm, and analyze its time and space complexities.
+ * 
+ * input:  text = "aa", pattern = "a"
+ * output: false
+ *
+ * input:  text = "aa", pattern = "aa"
+ * output: true
+ *
+ * input:  text = "abc", pattern = "a.c"
+ * output: true
+ *
+ * input:  text = "abbb", pattern = "ab*"
+ * output: true
+ *
+ * input:  text = "acd", pattern = "ab*c."
+ * output: true
+ */
+
 public class Mock_1_Interviewer {
 
 	static boolean isMatch(String text, String pattern) {
