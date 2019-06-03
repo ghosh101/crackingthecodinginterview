febc327d3ea6f4aacdd5eb2c6296a2bfdd3365b8
Suman Ghosh <ghosh.10@gmail.com>
2019-06-02 22:10:44 -0400

    partial solution to problem 12 of trees and graphs

---
+++ src/main/java/com/cracking/interview/treesgraphs/Problem_4_12_PathsWithSum.java
@@ -0,0 +1,61 @@
+package com.cracking.interview.treesgraphs;
+
+import java.util.HashSet;
+
+import com.cracking.interview.treesgraphs.Tree.Node;
+
+public class Problem_4_12_PathsWithSum {
+	
+	static int count = 0;
+
+	public static void main(String[] args) {
+		Tree tree = new Tree();
+		tree.root = new Node(10);
+		tree.root.left = new Node(5);
+		tree.root.left.left = new Node(3);
+		tree.root.left.left.left = new Node(3);
+		tree.root.left.left.right = new Node(-2);
+		tree.root.left.right = new Node(2);
+		tree.root.left.right.right = new Node(1);
+		tree.root.right = new Node(-3);
+		tree.root.right.right = new Node(11);
+		
+		int targetSum = 2;
+		//findPaths(tree.root, value);
+		countPathSums(tree.root, new HashSet<>(), targetSum, 0);
+		System.out.println(count);
+	}
+	
+	
+	private static void countPathSums(Node node, HashSet<Integer> set, int targetSum, int sum) {
+		if(node == null) return;
+		
+		sum = sum + node.value;
+		if(sum == targetSum) count++;
+		else if(set.contains(sum - targetSum)) count++;
+		else set.add(sum);
+		
+		countPathSums(node.left, set, targetSum, sum);
+		countPathSums(node.right, set, targetSum, sum);
+	}
+	
+	
+	private static void findPaths(Node root, int value) {
+		if(root == null) return;
+		
+		countSumPaths(root, value, 0);
+		findPaths(root.left, value);
+		findPaths(root.right, value);
+	}
+
+	private static void countSumPaths(Node root, int value, int sum) {
+		if(root == null) return;
+		
+		sum = sum + root.value;
+		if(sum == value) count++;
+		
+		countSumPaths(root.left, value, sum);
+		countSumPaths(root.right, value, sum);
+	}
+
+}
