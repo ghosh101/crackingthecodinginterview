d00ed4166aac1c261ffa10601ff011482bc03a4c
Suman Ghosh <ghosh.10@gmail.com>
2019-06-11 08:32:00 -0400

    comments

--- src/main/java/com/cracking/interview/sorting/QuickSort.java
+++ src/main/java/com/cracking/interview/sorting/QuickSort.java
@@ -22,8 +22,8 @@ public class QuickSort {
 	private static int partition(int[] arr, int l, int r) {
 		int pivot = arr[r];
 
-		int i = l-1;
-		int j = l;
+		int i = l-1; //saves the position after which the pivot element will be placed
+		int j = l; //used to iterate
 
 		while(j < r) {
 			if(arr[j] < pivot) {
